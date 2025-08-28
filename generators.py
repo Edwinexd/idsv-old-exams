dbe1bbb9cafbeb5f20ae8b2b5db6a12e0b387c6b
Edwin <60476129+Edwinexd@users.noreply.github.com>
2025-08-28 09:33:39 +0200

    Document current limitations of input, write multichoice generator

--- generators.py
+++ generators.py
@@ -1,5 +1,7 @@
+import logging
 from typing import Dict
 from models import LANGUAGE_CODE, Generator, Question, QuestionType
+from i18n import LANGUAGES
 
 class _GeneratorRegistry:
     _generators: Dict[QuestionType, Generator] = {}
@@ -16,22 +18,81 @@ class _GeneratorRegistry:
 registry = _GeneratorRegistry()
 
 # Short Answer
+# TODO: Abstract and add essay variant which is basically the same but longer space for answer
 class ShortAnswerGenerator(Generator):
     supported_type = QuestionType.sa
 
     def to_latex(self, question: Question, lang: LANGUAGE_CODE, with_answer: bool = False) -> str:
+        language = LANGUAGES[lang]
         content = question.content.get(lang)
         if not content:
-            return "\\textit{Content not available in the requested language.}\n\n"
+            logging.warning(f"Question {question.id} does not have content in language {lang}")
+            # return f"\\textit{{{language.content_not_available_language}}}\n\n"
+            return ""
 
         latex = f"\\subsection{{{content.question}}}\n\n"
         latex += f"\\label{{q:{question.id}:sa:{lang}:{with_answer}}}\n\n"
         if with_answer and content.answer:
-            latex += f"\\textbf{{Answer}}: {content.answer}\n\n"
+            latex += f"\\textbf{{{language.answer}}}: {content.answer}\n\n"
         else:
             # TODO Typable pdf text box?
             latex += "\\vspace{2cm}\n\n"  # Space for answer
+            latex += "\\noindent\\makebox[\\textwidth]{\\hrulefill}\n\n"  # Horizontal line
+            # jump to answer
+            latex += "\\vspace{1cm}\n\n"
+            latex += f"\\textit{{{language.answer}}}: \\autoref{{q:{question.id}:sa:{lang}:{True}}}\n\n"
 
         return latex
+
+# sc = "Single Choice" # generates a question with multiple alternatives, only one is correct
+
+class MultipleChoiceGenerator(Generator):
+    supported_type = QuestionType.mc
+
+    def to_latex(self, question: Question, lang: LANGUAGE_CODE, with_answer: bool = False) -> str:
+        language = LANGUAGES[lang]
+        content = question.content.get(lang)
+        if not content:
+            logging.warning(f"Question {question.id} does not have content in language {lang}")
+            return ""
+
+        latex = f"\\subsection{{{content.question}}}\n\n"
+        latex += f"\\label{{q:{question.id}:mc:{lang}:{with_answer}}}\n\n"
+
+        if not content.ans_alternatives:
+            logging.warning(f"Question {question.id} does not have answer alternatives for multiple choice")
+            return ""
+
+        latex += "\\begin{itemize}\n"
+        for alternative in content.ans_alternatives:
+            if with_answer and content.answer and alternative == content.answer:
+                latex += f"  \\item[$\\square$] \\textbf{{{alternative}}} \\hfill \\textbf{{{language.answer}}}\n"
+            else:
+                latex += f"  \\item[$\\square$] {alternative}\n"
+        latex += "\\end{itemize}\n\n"
+
+        if not with_answer:
+            latex += "\\vspace{1cm}\n\n"
+            latex += f"\\textit{{{language.answer}}}: \\autoref{{q:{question.id}:mc:{lang}:{True}}}\n\n"
+
+        return latex
+
+# mq = "Multi Question" # generates a questions
+# half of the mq questions isn't actually questions either but rather statements which cant be answered to the expected answer
+class MultiQuestionGenerator(Generator):
+    supported_type = QuestionType.mq
+
+    def to_latex(self, question: Question, lang: LANGUAGE_CODE, with_answer: bool = False) -> str:
+        logging.warning("MultiQuestionGenerator is not implemented yet.") # it contains two questions really, both will be generated
+        return ""
+
+
+# dq = "Drop Down"
+# dq is fucked in the provided CSV as no answer alternatives are given, ???
+
+# nq = "Number"
+
+
+
 
 registry.register_generator(ShortAnswerGenerator())
--- i18n.py
+++ i18n.py
@@ -6,3 +6,11 @@ class Language(BaseModel):
     answer: str
     content_not_available_language: str
 
+
+swedish = Language(name="se", answer="Svar", content_not_available_language="Frågan finns inte på detta språk")
+english = Language(name="en", answer="Answer", content_not_available_language="The question is not available in this language")
+
+LANGUAGES = {
+    "se": swedish,
+    "en": english,
+}
